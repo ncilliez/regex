b5686087132e9c2e259b1b9b299f5b115f2914ae
ncilliez <ncilliez@gmail.com>
2023-01-30 17:04:38 +0100

    mdp formulaire regex

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-let monElementnom = document.getElementById('name');
-let monElementPrenom = document.getElementById('prenom');
-let monElementMail = document.getElementById('mail');
-let bouton =document.getElementById('btnvalider');
-let cgu = document.getElementById('cgu');
+let bouton = document.getElementById('btn')
+var msgerreur ='';
+let nom = document.getElementById('nom')
+let idnom = document.getElementById('idnom')
+let prenom = document.getElementById('prenom')
+let idprenom = document.getElementById('idprenom')
+let email = document.getElementById('email')
+let idemail = document.getElementById('idemail')
+let password = document.getElementById('password')
+let idpassword = document.getElementById('idpassword')
+
+nom.addEventListener('blur', function(){
+    idnom.classList.remove('rouge')
+})
+prenom.addEventListener('blur', function(){
+    idprenom.classList.remove('rouge')
+})
+email.addEventListener('blur', function(){
+    idemail.classList.remove('rouge')
+})
+password.addEventListener('blur', function(){
+    idpassword.classList.remove('rouge')
+})
 
 
-
-bouton.addEventListener('click', function(){
-
-    if (monElementnom.value == "")                                  
-    { 
-        alert("Mettez votre nom."); 
-    } 
-    if (monElementPrenom.value == "")                                  
-    { 
-        alert("Mettez votre prenom."); 
-    } 
-    if(cgu.checked == false){
-            alert('Cochez le CGU')
+bouton.addEventListener('click', function(e){
+    if(nom.value ==""){
+        e.preventDefault();
+        msgerreur = msgerreur +"<div>nom</div>"
+        idnom.classList.add('rouge')
+    }  
+    if(prenom.value ==""){
+        e.preventDefault();
+        msgerreur = msgerreur +"<div>prenom</div>"
+        idprenom.classList.add('rouge')
     }
+    if(email.value ==""){
+        e.preventDefault();
+        msgerreur = msgerreur +"<div>email</div>"
+        idemail.classList.add('rouge')
+    }  
+    if(password.value ==""){
+        e.preventDefault();
+        msgerreur = msgerreur +"<div>password</div>"
+        idpassword.classList.add('rouge')
+    }   
 
-  })
-
- 
-
+    output.innerHTML= msgerreur;
+    msgerreur='';
+})
